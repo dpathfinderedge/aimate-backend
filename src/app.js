d402a7b6dc068052ebbf4a57a063cc7327190bf4
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-11-14 10:30:31 +0100

    fix error

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL, "http://localhost:5173"], credentials: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
